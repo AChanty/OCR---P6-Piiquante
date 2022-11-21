2885900dfb09533e3b1d98ee7a6f58a56518d99c
Anthony Chanty <anthony.chanty@hotmail.com>
2022-11-21 17:24:50 +0100

    feat add multer and multer middleware configs

---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+
+// configuration de multer
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => { // destination  d'enregistrement des fichiers
+    callback(null, 'images'); // nom du dossier en 2e argument
+  },
+  filename: (req, file, callback) => { // explique à multer quel nom de fichier utiliser
+    const name = file.originalname.split(' ').join('_'); // utilise le nom d'origine du fichier, y élimine les espaces en les remplacant par des underscore
+    const extension = MIME_TYPES[file.mimetype]; // génère l'extension du fichier
+    callback(null, name + Date.now() + '.' + extension); // création d'un nom unique pour le fichier (nom + date + '.' + extension du fichier)
+  }
+});
+
+module.exports = multer({storage: storage}).single('image');
