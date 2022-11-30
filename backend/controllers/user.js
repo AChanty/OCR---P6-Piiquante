247d05eaeeee0d374c628076a8b96a764c53a676
Anthony Chanty <anthony.chanty@hotmail.com>
2022-11-30 17:29:07 +0100

    feat add regex rules to createUser controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -12,22 +12,80 @@ const User = require('../models/user')
 require('dotenv').config()
 
 // création d'un nouvel utilisateur
-exports.createUser = (req, res, next) => {
-    // regex
-    bcrypt.hash(req.body.password, 10) // hache la donnée password pour la crypter, execute l'algorithme de hachage 10 fois pour créer un certain niveau de sécurité
-    .then(hash => {
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-        user.save() // enregistre l'utilisateur dans la base de données
-        .then(() => res.status(201).json({ message: 'Utilisateur enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }))
+// exports.createUser = (req, res, next) => {
+//     // regex
+//     bcrypt.hash(req.body.password, 10) // hache la donnée password pour la crypter, execute l'algorithme de hachage 10 fois pour créer un certain niveau de sécurité
+//     .then(hash => {
+//         const user = new User({
+//             email: req.body.email,
+//             password: hash
+//         });
+//         user.save() // enregistre l'utilisateur dans la base de données
+//         .then(() => res.status(201).json({ message: 'Utilisateur enregistré !' }))
+//         .catch(error => res.status(400).json({ error }));
+//     })
+//     .catch(error => res.status(500).json({ error }))
 
-    console.log(req.body);
+//     console.log(req.body);
+// };
+
+// création d'un nouvel utilisateur
+exports.createUser = (req, res, next) => {
+    let regexPassword = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/gm) // le mot de passe doit contenir au moins 5 caractères dont au moins un chiffre
+    let regexEmail = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}') // l'adresse email doit être dans un format email régulier
+    if (!regexPassword.test(req.body.password)) { // ne permet pas la création de l'utilisateur si le mot de passe n'est pas conforme au regex
+        console.log('mot de passe non valide')
+        return res.status(401).json({ message: 'Format de mot de passe non valide'});
+    } else if (!regexEmail.test(req.body.email)) { // ne permet pas la création de l'utilisateur si l'email n'est pas conforme au regex
+        console.log('email non valide')
+        return res.status(401).json({ message: 'Format de mail non valide'});
+    } else { // permet la création de l'utilisateur si les différents regex sont conformes
+        bcrypt.hash(req.body.password, 10) // hache la donnée password pour la crypter, execute l'algorithme de hachage 10 fois pour créer un certain niveau de sécurité
+            .then(hash => {
+                const user = new User({
+                    email: req.body.email,
+                    password: hash
+                });
+                user.save() // enregistre l'utilisateur dans la base de données
+                    .then(() => res.status(201).json({ message: 'Utilisateur enregistré !' }))
+                    .catch(error => res.status(400).json({ error }));
+            })
+            .catch(error => res.status(500).json({ error }))
+    }
 };
+
+// const express = require('express');
+// const app = express();
+// const { body, validationResult } = require('express-validator');
+// exports.createUser = (req, res, next) => {
+//     // regex
+//     // if (body('username').isEmail() && body('password').isLength({ min: 5 })) {
+//     body('username').isEmail(),
+//         // password must be at least 5 chars long
+//         body('password').isLength({ min: 5 }),
+//         (req, res) => {
+//             const errors = validationResult(req);
+//             if (!errors.isEmpty()) {
+//                 return res.status(400).json({ errors: errors.array() });
+//             } else {
+//             bcrypt.hash(req.body.password, 10) // hache la donnée password pour la crypter, execute l'algorithme de hachage 10 fois pour créer un certain niveau de sécurité
+//                 .then(hash => {
+//                     const user = new User({
+//                         email: req.body.email,
+//                         password: hash
+//                     });
+//                     user.save() // enregistre l'utilisateur dans la base de données
+//                         .then(() => res.status(201).json({ message: 'Utilisateur enregistré !' }))
+//                         .catch(error => res.status(400).json({ error }));
+//                 })
+//             }
+//     // } else {
+//     //     return res.status(401).json({ message: 'Entrées non valides'});
+//     // }
+//     // .catch(error => res.status(500).json({ error }))
+//     console.log(req.body);
+// };
+// }
 
 // connexion de l'utilisateur
 exports.connectUser = (req, res, next) => {
