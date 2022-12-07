2afc9556762ea985a488514e72607bba72544353
Anthony Chanty <anthony.chanty@hotmail.com>
2022-12-07 14:08:09 +0100

    feat add Joi on sauces/expressvalidator on signup

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -2,7 +2,9 @@
 
 // importe les différents modèles Mongoose dans l'application pour pouvoir les utiliser
 const Sauce = require('../models/sauce')
+
 const fs = require('fs');
+const Joi = require('joi');
 
 // renvoie un tableau de toutes les sauces de la base de données
 exports.displaySauces = (req, res, next) => {
@@ -20,7 +22,27 @@ exports.displaySauceId = (req, res, next) => {
 
 // permet de poster une sauce
 exports.createSauce = (req, res, next) => {
-    console.log(req.body);
+    const sauceSchema = Joi.object({ // règles mises en place via Joi demandant que chaque champs soit requis et ne soit pas vide
+        name: Joi.string()
+            .alphanum()
+            .min(1)
+            .required(),
+        manufacturer: Joi.string()
+            .alphanum()
+            .min(1)
+            .required(),
+        description: Joi.string()
+            .alphanum()
+            .min(1)
+            .required(),
+        mainPepper: Joi.string()
+            .alphanum()
+            .min(1)
+            .required(),
+        heat: Joi.number()
+            .required(),
+    })
+
     const sauceObject = JSON.parse(req.body.sauce)
     delete sauceObject._id;
     delete sauceObject.userId; // supprime la ligne userId de la requête
@@ -36,15 +58,88 @@ exports.createSauce = (req, res, next) => {
 
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`, // capture et enregistre l'image, en générant une url unique à partir du nom du fichier
     })
-    console.log(sauceObject);
-    console.log("url de l'image: " + req.file.filename);
-    console.log(`${req.protocol}://${req.get('host')}/images/${req.file.filename}`);
 
+    if (!sauceSchema.validate(sauceObject).error) { // si aucune erreur n'est retournée par Joi après les contrôles des champs
+        sauce.save() // enregistre la sauce et ses informations dans la base de données
+            .then(() => res.status(201).json({ message: 'Sauce créée !' }))
+            .catch(error => res.status(400).json({ error }));
 
-    sauce.save() // enregistre la sauce et ses informations dans la base de données
-        .then(() => res.status(201).json({ message: 'Sauce créée !' }))
-        .catch(error => res.status(400).json({ error }));
+    } else {
+        return res.status(400).json({ message: 'Format non valide' });
+    }
 };
+
+// permet la modification d'une sauce et son image de la base de données, seulement si celle-ci a été créée par le même utilisateur
+exports.modifySauce = (req, res, next) => {
+    const sauceSchema = Joi.object({ // règles mises en place via Joi demandant que chaque champs soit requis et ne soit pas vide
+        name: Joi.string()
+            .alphanum()
+            .min(1)
+            .required(),
+        manufacturer: Joi.string()
+            .alphanum()
+            .min(1)
+            .required(),
+        description: Joi.string()
+            .alphanum()
+            .min(1)
+            .required(),
+        mainPepper: Joi.string()
+            .alphanum()
+            .min(1)
+            .required(),
+        heat: Joi.number()
+            .required(),
+        userId: Joi.string()
+            .alphanum()
+            .required(),
+    })
+
+    Sauce.findOne({ _id: req.params.id })// utlisation de l'id recue en paramètre pour accéder à la sauce correspondante dans la base de données
+        .then(sauce => {
+            if (sauce.userId != req.auth.userId) { // refus de la requête si ce n'est pas le bon utilisateur
+                res.status(401).json({ message: 'Action non autorisée' })
+            } else {
+                if (!sauceSchema.validate(req.body).error) { // si aucune erreur n'est retournée par Joi après les contrôles des champs
+                    // met à jour la sauce si c'est le bon utilisateur
+                    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+                        .then(() => { res.status(200).json({ message: 'Sauce modifiée !' }) })
+                        .catch(error => res.status(401).json({ error }));
+                } else {
+                    return res.status(400).json({ message: 'Format non valide' });
+                }
+            }
+        })
+        .catch(error => res.status(500).json({ error }));
+};
+
+// // permet de poster une sauce
+// exports.createSauce = (req, res, next) => {
+//     console.log(req.body);
+//     const sauceObject = JSON.parse(req.body.sauce)
+//     delete sauceObject._id;
+//     delete sauceObject.userId; // supprime la ligne userId de la requête
+//     const sauce = new Sauce({
+//         ...sauceObject,
+//         userId: req.auth.userId,
+
+//         // initialise les likes et dislikes de la sauce à 0, et les usersLiked et usersDisliked avec des tableaux vides
+//         likes: 0,
+//         dislikes: 0,
+//         usersLiked: [],
+//         usersDisliked: [],
+
+//         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`, // capture et enregistre l'image, en générant une url unique à partir du nom du fichier
+//     })
+//     console.log(sauceObject);
+//     console.log("url de l'image: " + req.file.filename);
+//     console.log(`${req.protocol}://${req.get('host')}/images/${req.file.filename}`);
+
+
+//     sauce.save() // enregistre la sauce et ses informations dans la base de données
+//         .then(() => res.status(201).json({ message: 'Sauce créée !' }))
+//         .catch(error => res.status(400).json({ error }));
+// };
 
 // permet la suppression d'une sauce et son image de la base de données, seulement si celle-ci a été créée par le même utilisateur
 exports.deleteSauce = (req, res, next) => {
@@ -64,20 +159,24 @@ exports.deleteSauce = (req, res, next) => {
         .catch(error => res.status(500).json({ error }));
 }
 
+
 // permet la modification d'une sauce et son image de la base de données, seulement si celle-ci a été créée par le même utilisateur
-exports.modifySauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })// utlisation de l'id recue en paramètre pour accéder à la sauce correspondante dans la base de données
-        .then(sauce => {
-            if (sauce.userId != req.auth.userId) { // refus de la requête si ce n'est pas le bon utilisateur
-                res.status(401).json({ message: 'Action non autorisée' })
-            } else { // met à jour la sauce si c'est le bon utilisateur
-                Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-                    .then(() => { res.status(200).json({ message: 'Sauce modifiée !' }) })
-                    .catch(error => res.status(401).json({ error }));
-            }
-        })
-        .catch(error => res.status(500).json({ error }));
-};
+// exports.modifySauce = (req, res, next) => {
+//     Sauce.findOne({ _id: req.params.id })// utlisation de l'id recue en paramètre pour accéder à la sauce correspondante dans la base de données
+//         .then(sauce => {
+//             if (sauce.userId != req.auth.userId) { // refus de la requête si ce n'est pas le bon utilisateur
+//                 res.status(401).json({ message: 'Action non autorisée' })
+//             } else { // met à jour la sauce si c'est le bon utilisateur
+//                 Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+//                     .then(() => { res.status(200).json({ message: 'Sauce modifiée !' }) })
+//                     .catch(error => res.status(401).json({ error }));
+//             }
+//         })
+//         .catch(error => res.status(500).json({ error }));
+// };
+
+
+
 // Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
 // // .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
 // // .catch(error => res.status(400).json({ error }));
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 // importe les différents modèles Mongoose dans l'application pour pouvoir les utiliser
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.createUser);
+router.post('/signup', body('email').isEmail(), body('password').isStrongPassword(), userCtrl.createUser); // vérification des champs email et password avec Express Validator
 router.post('/login', userCtrl.connectUser);
-// router.post('/login', limiter, userCtrl.connectUser);
 
 
 module.exports = router;
